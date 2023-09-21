a62dea4fccc7bbfba91063f678a58eae6d49f49d
Long luong <137338155+GNoL7@users.noreply.github.com>
2023-09-21 13:54:29 -0400

    add pycurl, update curl request function, have not test function yet

--- buzz.py
+++ buzz.py
@@ -1,13 +1,41 @@
 #!/usr/bin/python3
-import requests
+import requests , pycurl
 
 
 url = 'https://www.eventbriteapi.com/v3/users/me/?token=4B2WKIGMY7VW2T356IXQ'
 
-##function for intergrate pyscript with website (KIR)
-#def pyscript_integration():
 
+def curl_request():
+    # Create a Curl object
+    curl = pycurl.Curl()
 
+    # Set the URL
+    url = "https://www.eventbriteapi.com/v3/organizations/4B2WKIGMY7VW2T356IXQ/events/"
+    curl.setopt(pycurl.URL, url)
+   
+    # Set the HTTP method to GET
+    curl.setopt(pycurl.HTTPGET, 1)
+
+    # Set request headers
+    headers = [
+        "Authorization: Bearer 4B2WKIGMY7VW2T356IXQ",
+    ]
+    curl.setopt(pycurl.HTTPHEADER, headers)
+
+    # Perform the HTTP request
+    curl.perform()
+
+    # Get the response code
+    response_code = curl.getinfo(pycurl.RESPONSE_CODE)
+    print("Response Code:", response_code)
+
+    # Get the response body
+    response_body = curl.getinfo(pycurl.RESPONSE_BODY)
+    print("Response Body:", response_body)
+
+    # Clean up
+    curl.close()
+    
 
 
 
@@ -17,5 +45,5 @@ def api_call(url):
     print(respone.status_code)
     return respone.json()
 
-a =api_call(url)
+a = api_call(url)
 print(a)

a47a73efb5f93cd282f8c9c5c8363ba08147d841
Long luong <137338155+GNoL7@users.noreply.github.com>
2023-09-21 18:58:38 -0400

    remove replace old api key with new public API- ticketmaster

--- buzz.py
+++ buzz.py
@@ -1,49 +1,18 @@
 #!/usr/bin/python3
-import requests , pycurl
+import requests , pycurl, json
 
-
-url = 'https://www.eventbriteapi.com/v3/users/me/?token=4B2WKIGMY7VW2T356IXQ'
-
-
-def curl_request():
-    # Create a Curl object
-    curl = pycurl.Curl()
-
-    # Set the URL
-    url = "https://www.eventbriteapi.com/v3/organizations/4B2WKIGMY7VW2T356IXQ/events/"
-    curl.setopt(pycurl.URL, url)
-   
-    # Set the HTTP method to GET
-    curl.setopt(pycurl.HTTPGET, 1)
-
-    # Set request headers
-    headers = [
-        "Authorization: Bearer 4B2WKIGMY7VW2T356IXQ",
-    ]
-    curl.setopt(pycurl.HTTPHEADER, headers)
-
-    # Perform the HTTP request
-    curl.perform()
-
-    # Get the response code
-    response_code = curl.getinfo(pycurl.RESPONSE_CODE)
-    print("Response Code:", response_code)
-
-    # Get the response body
-    response_body = curl.getinfo(pycurl.RESPONSE_BODY)
-    print("Response Body:", response_body)
-
-    # Clean up
-    curl.close()
-    
-
-
-
-## for now api_call function tested and it work. 
+api_key = "P8ahPHp6GiDVgvIVSzqV2l3gFGxLAp3d"
+base_url = "https://app.ticketmaster.com/discovery/v2/"
+test_event="events.json"
+test_url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=P8ahPHp6GiDVgvIVSzqV2l3gFGxLAp3d"
 def api_call(url):
     respone = requests.get(url)
     print(respone.status_code)
     return respone.json()
 
-a = api_call(url)
-print(a)
+
+
+
+##testing
+a = api_call(test_url)
+print(json.dumps(a, indent=1))

d7f5b31620d285c362193324d6ea3fc55219b1f3
Long luong <137338155+GNoL7@users.noreply.github.com>
2023-09-22 18:40:43 -0400

    tested call_api function, able to get information from ticket master

--- buzz.py
+++ buzz.py
@@ -2,17 +2,33 @@
 import requests , pycurl, json
 
 api_key = "P8ahPHp6GiDVgvIVSzqV2l3gFGxLAp3d"
-base_url = "https://app.ticketmaster.com/discovery/v2/"
-test_event="events.json"
-test_url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=P8ahPHp6GiDVgvIVSzqV2l3gFGxLAp3d"
+base_url = "https://app.ticketmaster.com/discovery/v2"
+endpoint="/events.json"
+city='Boston'
+
+
+url = f"{base_url}{endpoint}?apikey={api_key}&countryCode=US&city={city}"
+
+
+
+#call api
 def api_call(url):
     respone = requests.get(url)
     print(respone.status_code)
-    return respone.json()
+    data = respone.json()
+    events = data.get("_embedded", {}).get("events", [])
+    return events
 
 
 
-
-##testing
-a = api_call(test_url)
-print(json.dumps(a, indent=1))
+# "https://app.ticketmaster.com/discovery/v2/events.json?apikey=P8ahPHp6GiDVgvIVSzqV2l3gFGxLAp3d&countryCode=US&city=Boston"
+# testing
+events_list = api_call(url)
+index = 0  
+while index < len(events_list):
+    event = events_list[index]
+    print(f"Event Name: {event['name']}")
+    print(f"Event id: {event['id']}")
+    print(f"Event URL: {event['url']}")
+    print("-----------------------")
+    index += 1 

125558df9f7e8cf7a5235516d8edb16d8a81fc5d
Long luong <137338155+GNoL7@users.noreply.github.com>
2023-10-22 15:21:12 -0400

    testing cleanup dup files

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+import sqlite3
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+"""@app.route('/')
+def index():
+    return render_template('index.html')"""
+
+def get_db_connection():
+    conn = sqlite3.connect('events.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM processed_events').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)

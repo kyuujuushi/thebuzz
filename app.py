27bc4e236a53c263cdd475dab698475626685d74
kyuujuushi <alypompom28@gmail.com>
2023-11-09 17:50:07 -0500

    added a button that runs the mk_cal function to make a calendar file

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from icalendar import Calendar, Event
+from datetime import datetime
+from pathlib import Path
+import os
+import pytz
 
 app = Flask(__name__)
 
@@ -8,11 +13,44 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def mk_cal (name, date):
+    # init the calendar
+    cal = Calendar()
+
+    #making of the event and putting it into the calendar object
+    event = Event()
+    event.add('name', name)
+    #date is currently a dummy date will need to figure out later
+    event.add('dtstart', datetime(2023, 10, 31, 20, 0, 0, tzinfo=pytz.utc))
+    cal.add_component(event)
+    # commented out currently so that we'll later figure out file shit
+    '''
+    # making of the calendar file
+    f = open(name + ".ics", "wb")
+    f.write(cal.to_ical())
+    f.close()
+    '''
+    return "mk_cal function has been called. this is a test"
+
 @app.route('/')
 def index():
+    #connecting to the database and fetching the data
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM processed_events').fetchall()
+
+    #variables required for mk_cal function. put in the event name and date in these
+    # instead of the test stuff
+    eventname = "test"
+    eventdate = 0
+
+    #closing of SQL
     conn.close()
+
+    #flask checking for 
+    if request.method == 'POST':
+        if request.form['Get Calendar'] == 'getCal':
+            mk_cal(eventname, eventdate)
+
     return render_template('index.html', posts=posts)
 
 if __name__ == '__main__':

258b8b10da110a81c85141b2baa70933d13471e8
Augustus123 <romanoaugustus63@gmail.com>
2023-12-06 20:52:32 -0500

    added the share button, doesnt work yet

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,7 @@ const mes = encodeURIComponent('Look at These Events!');
 const title = encodeURIComponent('Article or Post Title Here');
 
 const reddit = document.querySelector('.reddit');
-reddit.href = 'https://reddit.com/submit?url=[post-url]&title=[post-title]'
-
+reddit.href = `http://www.reddit.com/submit?url=${link}&title=${title}`;
 const Twitter
 
 const linkedin
